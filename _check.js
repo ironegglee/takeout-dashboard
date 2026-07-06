72ee50fd1d22a379bbdd4d431e0a5e3bd7537481
ironegglee <ironegglee@users.noreply.github.com>
2026-07-06 10:26:12 +0800

    feat: 优化导出报告结构，按优先级分组，只导出问题门店

--- _check.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const h = fs.readFileSync('dashboard/index.html', 'utf8');
-console.log('leader select:', h.includes('id="gf-leader"'));
-console.log('onAreaMgrChange:', h.includes('function onAreaMgrChange'));
-console.log('gfState.leader:', h.includes("leader:'all'"));
--- _verify.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const h = fs.readFileSync('index.html', 'utf8');
-console.log('leader select:', h.includes('id="gf-leader"'));
-console.log('onAreaMgrChange:', h.includes('function onAreaMgrChange'));
-console.log('gfState.leader:', h.includes("leader:'all'"));
-console.log('EMBEDDED_DATE_END:', (h.match(/EMBEDDED_DATE_END = "([^"]+)"/)||[])[1]);
-console.log('build-time:', (h.match(/<meta name="build-time" content="([^"]+)"/)||[])[1]);
